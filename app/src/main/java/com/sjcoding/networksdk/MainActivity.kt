9c30816b9992efafe917a5b2dadc41f1cfcfd8ab
Apple <apple@Credencs-MacBook-Pro.local>
2024-05-14 20:32:55 +0530

    try 2

--- app/src/main/java/com/sjcoding/networksdk/MainActivity.kt
+++ app/src/main/java/com/sjcoding/networksdk/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        NetworkSD.initialize("https://jsonplaceholder.typicode.com/")
+        NetworkSDK.initialize("https://jsonplaceholder.typicode.com/")
 
         lifecycleScope.launch {
             val commentsRepository = NetworkModule.networkRepository
