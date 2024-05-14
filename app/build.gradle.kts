3c1cd1303584757a2f17074fc4ccd887caa43fe9
Apple <apple@Credencs-MacBook-Pro.local>
2024-05-14 20:25:21 +0530

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation(project(":networksdk"))
+//    implementation(project(":networksdk"))
+    implementation("com.github.Shubhamj08:networksdk:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
